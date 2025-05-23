578ddef33383bdccaa94d0f131fa7f4c91f94a2d
ameerhamza1490 <146176047+ameerhamza1490@users.noreply.github.com>
2025-05-23 13:28:06 +0500

    Update Step.jsx

--- src/Components/Step.jsx
+++ src/Components/Step.jsx
@@ -14,7 +14,7 @@ const Step = ({ step, setStep }) => {
       <div className="px-7 py-10 flex justify-center items-center flex-col gap-8  rounded-2xl shadow-xl/30 bg-gray-200">
         <h1 className="font-bold text-4xl">Step : {step}</h1>
 
-        <div className="">
+        <div className="flex justify-center items-center space-x-4">
           <button
             className="border-2 border-black px-4 py-2 cursor-pointer rounded-2xl  mr-3"
             onClick={decHandle}
